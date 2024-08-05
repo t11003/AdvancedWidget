2398db5f10fd7a7e6fd04dcec616194cc7aafa39
t11003 <t11003t11003@gmail.com>
2024-08-05 11:27:48 +0800

    Complete spinner calculator

--- SpinnerCalculator/app/src/main/java/com/example/spinnercalculator/MainActivity.java
+++ SpinnerCalculator/app/src/main/java/com/example/spinnercalculator/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.spinnercalculator;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +13,9 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        AdapterView.OnItemSelectedListener {
+    private double num1, num2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,47 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        EditText etNum1 = (EditText) findViewById(R.id.num1);
+        etNum1.setText("0");
+        EditText etNum2 = (EditText) findViewById(R.id.num2);
+        etNum2.setText("0");
+
+        Spinner op = (Spinner) findViewById(R.id.spinner);
+        op.setOnItemSelectedListener(this);
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        // 取得數字
+        EditText etNum1 = (EditText)findViewById(R.id.num1);
+        num1 = Double.parseDouble(String.valueOf(etNum1.getText()));
+        EditText etNum2 = (EditText)findViewById(R.id.num2);
+        num2 = Double.parseDouble(String.valueOf(etNum2.getText()));
+
+        // 取得操作方法的字串
+        String[] operations = getResources().getStringArray(R.array.operation);
+        double answer = 0.0;
+        switch (operations[i]){
+            case "+":
+                answer = num1 + num2;
+                break;
+            case "-":
+                answer = num1 - num2;
+                break;
+            case "*":
+                answer = num1 * num2;
+                break;
+            case "/":
+                answer = num1 / num2;
+                break;
+        }
+        TextView result = (TextView) findViewById(R.id.result);
+        result.setText(String.valueOf(answer));
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
