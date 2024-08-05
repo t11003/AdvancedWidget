6d55aef256b85bd8796545b78fe7242fa4fdd9ea
t11003 <t11003t11003@gmail.com>
2024-08-05 10:29:35 +0800

    用一個按鈕，透過spinner的getSelectedItemPosition()方法取得使用者選擇的選項index，最後將結果顯示在tvResult這個TextView．

--- Spinner/app/src/main/java/com/example/spinner/MainActivity.java
+++ Spinner/app/src/main/java/com/example/spinner/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.spinner;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +23,15 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void buttonClick(View view){
+        Spinner sp = (Spinner) findViewById(R.id.spinner);
+        // 找到選項的array
+        String[] fruits = getResources().getStringArray(R.array.fruit);
+        int index = sp.getSelectedItemPosition();
+
+        TextView result = (TextView) findViewById(R.id.tvResult);
+        result.setText("您選的是：" + fruits[index]);
     }
 }
