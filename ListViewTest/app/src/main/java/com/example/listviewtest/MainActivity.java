a8041a5c28686d40ecaf83d7394e6a35eb574f76
t11003 <t11003t11003@gmail.com>
2024-08-07 11:14:41 +0800

    透過自訂ArrayAdapter的方式，讓ListView的內容能自動變化．

--- ListViewTest/app/src/main/java/com/example/listviewtest/MainActivity.java
+++ ListViewTest/app/src/main/java/com/example/listviewtest/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.listviewtest;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -14,7 +15,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+    private ListView cityListView;
+    private List<String> cities = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,17 +33,28 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        ListView lv = (ListView) findViewById(R.id.cityListView);
-        lv.setOnItemClickListener(this);
+        cityListView = (ListView) findViewById(R.id.cityListView);
+        setCities();
+        cityListView.setOnItemClickListener(this);
+    }
+
+    private void setCities(){
+        cities = new ArrayList<>(Arrays.asList(getResources().getStringArray(R.array.cities)));
+        cities.add("Hualien");
+        cities.add("Taitung");
+        cities.add("Hsinchu");
+        cities.add("Changhua");
+        // new ArrayAdapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, cities);
+        cityListView.setAdapter(adapter);
     }
 
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
         TextView tv = (TextView) findViewById(R.id.textView);
-        String[] citiesArray = getResources().getStringArray(R.array.cities);
 
         AlertDialog.Builder dialog = new AlertDialog.Builder(this);
-        dialog.setTitle("城市").setMessage("您選擇的是：" + citiesArray[i]).setCancelable(true);
+        dialog.setTitle("城市").setMessage("您選擇的是：" + cities.get(i)).setCancelable(true);
         dialog.setPositiveButton("確定", null);
         dialog.setNeutralButton("取消", null);
         dialog.setNegativeButton("放棄", null);
