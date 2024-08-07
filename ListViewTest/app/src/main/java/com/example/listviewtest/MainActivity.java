1ee947101725dffc5fa558d0be0d3d46d37aa5a1
t11003 <t11003t11003@gmail.com>
2024-08-07 10:34:03 +0800

    利用onItemClick方法將使用者點擊的項目用TextView, Toast, Dialog方式呈現．

--- ListViewTest/app/src/main/java/com/example/listviewtest/MainActivity.java
+++ ListViewTest/app/src/main/java/com/example/listviewtest/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.listviewtest;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,27 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        ListView lv = (ListView) findViewById(R.id.cityListView);
+        lv.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        TextView tv = (TextView) findViewById(R.id.textView);
+        String[] citiesArray = getResources().getStringArray(R.array.cities);
+
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("城市").setMessage("您選擇的是：" + citiesArray[i]).setCancelable(true);
+        dialog.setPositiveButton("確定", null);
+        dialog.setNeutralButton("取消", null);
+        dialog.setNegativeButton("放棄", null);
+        dialog.show();
+
+//        Toast.makeText(
+//                this,
+//                "您選擇的是：" + citiesArray[i],
+//                Toast.LENGTH_SHORT
+//        ).show();
+//        tv.setText("您選擇的是：" + citiesArray[i]);
     }
 }
